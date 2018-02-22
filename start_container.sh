f6a66aaf2301c658134f817ac366dec1a04b763d
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2018-02-22 13:10:46 -0500

    draft of grid scripts

--- start_container.sh
+++ start_container.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-singularity shell -B /media/hdd1:/media/hdd1 /media/hdd1/larbys/containers/singularity-dllee-unified-taggerv2beta-20171121.img
+# ON MEITNER
+#CONTAINER=/media/hdd1/larbys/containers/singularity-dllee-unified-taggerv2beta-20171121.img
+#singularity shell -B /media/hdd1:/media/hdd1 $CONTAINER
+
+# ON TUFTS CLUSTER
+#CONTAINER=/cluster/kappa/90-days-archive/wongjiradlab/larbys/images/dllee_unified/singularity-dllee-intertool-20180216.img
+#module load singularity
+#singularity shell $CONTAINER
+
